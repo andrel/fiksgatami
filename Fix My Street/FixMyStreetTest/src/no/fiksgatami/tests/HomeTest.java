aaecca7f8291f062be6db6a5bb484a6bfb97047d
André Lindhjem <andre@lindhjem.net>
2013-04-21 21:09:01 +0200

    Add android test project and simple test case.

---
+++ Fix My Street/FixMyStreetTest/src/no/fiksgatami/tests/HomeTest.java
@@ -0,0 +1,73 @@
+package no.fiksgatami.tests;
+
+import android.app.Instrumentation;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.view.KeyEvent;
+import android.widget.Button;
+import android.widget.EditText;
+import no.fiksgatami.R;
+import no.fiksgatami.activities.Home;
+import no.fiksgatami.components.CameraButton;
+
+import java.util.Locale;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class no.fiksgatami.activities.HomeTest \
+ * no.fiksgatami.tests/android.test.InstrumentationTestRunner
+ */
+public class HomeTest extends ActivityInstrumentationTestCase2<Home> {
+
+    private Home mActivity;
+    private CameraButton mCameraButton;
+    private Instrumentation mInstrumentation;
+    private EditText textSubmissionTitle;
+    private Button mGalleryButton;
+    private static final String LOG_TAG = HomeTest.class.getSimpleName();
+
+    public HomeTest() {
+        super("no.fiksgatami", Home.class);
+    }
+
+    protected void setUp() throws Exception {
+        setActivityInitialTouchMode(false);
+        mInstrumentation = getInstrumentation();
+        mActivity = getActivity();
+
+        mCameraButton = (CameraButton) mActivity.findViewById(R.id.camera_button);
+        mGalleryButton = (Button) mActivity.findViewById(R.id.gallery_button);
+        textSubmissionTitle = (EditText) mActivity.findViewById(R.id.submission_title);
+    }
+
+    public void testPreConditions() throws Exception {
+        assertTrue(mCameraButton.isShown());
+        assertTrue(mCameraButton.isEnabled());
+        assertTrue(mGalleryButton.isShown());
+        assertTrue(mGalleryButton.isEnabled());
+        assertTrue(textSubmissionTitle.isEnabled());
+    }
+
+    public void testTextFieldContents() throws Exception {
+        String expectedTitle = mActivity.getText(R.string.submission_title).toString();
+        String actualTitle = textSubmissionTitle.getHint().toString();
+
+        assertEquals("Unexpected text field contents.", expectedTitle, actualTitle);
+    }
+
+    public void testTextFieldContentsChanges() throws Exception {
+        TouchUtils.tapView(this, textSubmissionTitle);
+        sendKeys(KeyEvent.KEYCODE_F);
+        sendKeys(KeyEvent.KEYCODE_O);
+        sendKeys(KeyEvent.KEYCODE_O);
+
+        String actualTitle = textSubmissionTitle.getText().toString().toUpperCase(Locale.getDefault());
+
+        assertEquals("Unexpected text field contents", "FOO", actualTitle);
+    }
+}
